79450d5295a2856d9671d346046c85ada4727426
Andrew Gardner <agardner@securenettech.com>
2020-03-06 13:27:55 -0700

    Convert Template.

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
+    <Hello msg="Welcome to HabitSmasher"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Hello from './components/Hello.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Hello
   }
 }
+
 </script>
 
 <style>
---
+++ src/components/Hello.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="hello">
+    <img alt="HabitSmasher" src="https://www.ssbwiki.com/images/2/2b/LinkHeadSSBUWebsite.png">
+    <h1>{{ msg }}</h1>
+    <p>
+      You suck at mixing up your gameplay. <span style='font-weight:bold;'>You keep getting read like a book!</span><br><br>
+      HabitSmasher lets you <span style='font-weight:bold;'>build a practice profile</span> with all your best moves, combos and mixups<br><br>
+      then <span style='font-weight:bold;'>prompts you while you practice</span> to try the ones you always forget.<br>
+    </p>
+  </div>
+</template>
+
+<script>
+
+export default 
+{
+  name: 'Hello',
+  props: {
+    msg: String
+  }
+}
+
+</script>
+
+
+<style scoped>
+
+</style>
